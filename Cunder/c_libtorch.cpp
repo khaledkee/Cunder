d58154d39d8299f4ba0184bb77b00da87725f523
Stohy <57235472+AhmedStohy@users.noreply.github.com>
2023-02-20 07:08:22 -0800

    Cunder-1 : Add cunder tensor wrapper (#1)
    * add cunder tesnor wrapper
    
    * fix feteching data from cunder tensor , add more data wrapper and add example playground
    
    * remove <torch/all.h> and <torch/script.h> from c_libtorch.h
    
    add cunder_tensor_from_data_wrap and cunder_tensor_from_data functions
    add cunder_tensor_to
    add cunder_tensor_print
    use Torch_Version struct
    
    * remove unnecessary attributes
    
    add type getters
    
    * fix unittests
    
    * add tensor accessors
    
    ---------
    
    Co-authored-by: Khaled KEE Fadel <khaledkee0@gmail.com>

--- Cunder/c_libtorch.cpp
+++ Cunder/c_libtorch.cpp
@@ -1,20 +1,327 @@
 #include "c_libtorch.h"
+#include <torch/all.h>
+#include <torch/script.h>
 
-//#include <torch/all.h>
-//#include <torch/script.h>
-
-namespace
+namespace cunder
 {
-	Torch_Version
-	c_torch_version()
+	inline static bool
+	is_valid_dtype(Cunder_DType dtype)
 	{
-		return Torch_Version{};
-		// TODO: defined in torch/all.h
-//		return Torch_Version{TORCH_VERSION_MAJOR, TORCH_VERSION_MINOR, TORCH_VERSION_PATCH};
+		if ((dtype == Cunder_Uint8) || (dtype == Cunder_Int8) || (dtype == Cunder_Int16) || (dtype == Cunder_Int32) || (dtype == Cunder_Int64) ||
+			(dtype == Cunder_Float32) || (dtype == Cunder_Float64))
+		{
+			return true;
+		}
+
+		return false;
 	}
-} // namespace
+
+	// Get torch native dtype.
+	constexpr c10::ScalarType
+	get_libtorch_dtype(Cunder_DType dtype)
+	{
+		switch (dtype)
+		{
+			case Cunder_Uint8:
+				return torch::kUInt8;
+
+			case Cunder_Int8:
+				return torch::kInt8;
+
+			case Cunder_Int16:
+				return torch::kInt16;
+
+			case Cunder_Int32:
+				return torch::kInt32;
+
+			case Cunder_Int64:
+				return torch::kInt64;
+
+			case Cunder_Float32:
+				return torch::kFloat32;
+
+			case Cunder_Float64:
+				return torch::kFloat64;
+
+			case Cunder_Invalid:
+			default:
+				throw std::invalid_argument("Unknown dtype");
+		}
+	}
+
+	// Get torch native dtype.
+	constexpr Cunder_DType
+	get_cunder_dtype(c10::ScalarType dtype)
+	{
+		switch (dtype)
+		{
+			case torch::kUInt8:
+				return Cunder_Uint8;
+
+			case torch::kInt8:
+				return Cunder_Int8;
+
+			case torch::kInt16:
+				return Cunder_Int16;
+
+			case torch::kInt32:
+				return Cunder_Int32;
+
+			case torch::kInt64:
+				return Cunder_Int64;
+
+			case torch::kFloat32:
+				return Cunder_Float32;
+
+			case torch::kFloat64:
+				return Cunder_Float64;
+
+			default:
+				return Cunder_Invalid;
+		}
+	}
+
+	constexpr int
+	get_dtype_size(Cunder_DType dtype)
+	{
+		switch (dtype)
+		{
+			case Cunder_Invalid:
+				return 0;
+
+			case Cunder_Uint8:
+			case Cunder_Int8:
+				return 8;
+
+			case Cunder_Int16:
+				return 16;
+
+			case Cunder_Int32:
+			case Cunder_Float32:
+				return 32;
+
+			case Cunder_Int64:
+			case Cunder_Float64:
+				return 64;
+		}
+	}
+} // namespace cunder
 
 extern "C"
 {
+struct Cunder_Tensor
+{
+	torch::Tensor tensor;
+};
+
+Torch_Version
+cunder_torch_version()
+{
+	return Torch_Version{TORCH_VERSION_MAJOR, TORCH_VERSION_MINOR, TORCH_VERSION_PATCH};
+}
+
+int
+cunder_tensor_free(Cunder_Tensor *obj)
+{
+	if (obj == nullptr)
+		return -1;
+
+	free(obj);
+
+	return 0; // success
+}
+
+inline static Cunder_Tensor *
+_cunder_check_initialization_param(int ndim, const int *shape, Cunder_DType dtype)
+{
+	if (ndim < 1)
+		return nullptr;
+
+	if (shape == nullptr)
+		return nullptr;
+
+	if (!cunder::is_valid_dtype(dtype))
+		return nullptr;
+
+	return new Cunder_Tensor();
+}
+
+Cunder_Tensor *
+cunder_tensor_ones(int ndim, const int *shape, Cunder_DType dtype)
+{
+	auto tensor = _cunder_check_initialization_param(ndim, shape, dtype);
+	if (tensor == nullptr)
+		return nullptr;
+
+	std::vector<int64_t> vshape;
+	for (int i = 0; i < ndim; i++)
+		vshape.push_back(shape[i]);
+
+	tensor->tensor = torch::ones(vshape, cunder::get_libtorch_dtype(dtype));
+
+	return tensor;
+}
+
+Cunder_Tensor *
+cunder_tensor_zeros(int ndim, const int *shape, Cunder_DType dtype)
+{
+	auto tensor = _cunder_check_initialization_param(ndim, shape, dtype);
+	if (tensor == nullptr)
+		return nullptr;
+
+	std::vector<int64_t> vshape;
+	for (int i = 0; i < ndim; i++)
+		vshape.push_back(shape[i]);
+
+	tensor->tensor = torch::zeros(vshape, cunder::get_libtorch_dtype(dtype));
+
+	return tensor;
+}
+
+Cunder_Tensor *
+cunder_tensor_eye(int n, Cunder_DType dtype)
+{
+	if (!cunder::is_valid_dtype(dtype))
+	{
+		return nullptr;
+	}
+
+	auto *tensor = new Cunder_Tensor();
+	tensor->tensor = torch::eye(n, cunder::get_libtorch_dtype(dtype));
+	return tensor;
+}
+
+Cunder_Tensor *
+cunder_tensor_range(int start, int end, int step, Cunder_DType dtype)
+{
+	if (!cunder::is_valid_dtype(dtype))
+	{
+		return nullptr;
+	}
+
+	auto *tensor = new Cunder_Tensor();
+	tensor->tensor = torch::range(start, end, step, cunder::get_libtorch_dtype(dtype));
+	return tensor;
+}
+
+Cunder_Tensor *
+cunder_tensor_from_data_wrap(int ndim, const int *shape, void *data, Cunder_DType dtype)
+{
+	auto tensor = _cunder_check_initialization_param(ndim, shape, dtype);
+	if (tensor == nullptr)
+		return nullptr;
+
+	std::vector<int64_t> vshape;
+	for (int i = 0; i < ndim; i++)
+		vshape.push_back(shape[i]);
+
+	tensor->tensor = torch::from_blob(data, vshape, torch::TensorOptions(cunder::get_libtorch_dtype(dtype)));
+	return tensor;
+}
+
+Cunder_Tensor *
+cunder_tensor_from_data(int ndim, const int *shape, void *data, Cunder_DType dtype, void (*free)(void *))
+{
+	auto tensor = _cunder_check_initialization_param(ndim, shape, dtype);
+	if (tensor == nullptr)
+		return nullptr;
+
+	std::vector<int64_t> vshape;
+	for (int i = 0; i < ndim; i++)
+		vshape.push_back(shape[i]);
+
+	tensor->tensor = torch::from_blob(data, vshape, free, torch::TensorOptions(cunder::get_libtorch_dtype(dtype)));
+
+	return tensor;
+}
+
+void
+cunder_tensor_to(Cunder_Tensor *tensor, Cunder_DType dtype)
+{
+	if (tensor == nullptr)
+		return;
+	tensor->tensor = tensor->tensor.toType(cunder::get_libtorch_dtype(dtype));
+}
+
+void
+cunder_tensor_print(const Cunder_Tensor *tensor)
+{
+	print(tensor->tensor);
+	printf("\n");
+}
+
+
+Cunder_DType
+cunder_tensor_type(const Cunder_Tensor *tensor)
+{
+	return cunder::get_cunder_dtype(tensor->tensor.scalar_type());
+}
+
+int64_t
+cunder_tensor_ndim(const Cunder_Tensor *tensor)
+{
+	return tensor->tensor.dim();
+}
+
+void
+cunder_tensor_shape(const Cunder_Tensor *tensor, int64_t ndim, int64_t *out_shape)
+{
+	int64_t tensor_dims = tensor->tensor.dim();
+	for (int64_t i = 0; i < ndim && i < tensor_dims; ++i)
+		out_shape[i] = tensor->tensor.size(i);
+}
+
+int64_t
+cunder_tensor_numel(const Cunder_Tensor *tensor)
+{
+	return tensor->tensor.numel();
+}
+
+int64_t
+cunder_tensor_dim_size(const Cunder_Tensor *tensor, int64_t dim)
+{
+	return tensor->tensor.size(dim);
+}
+
+const uint8_t *
+cunder_tensor_accessor_u8(const Cunder_Tensor *tensor)
+{
+	return tensor->tensor.data_ptr<uint8_t>();
+}
+
+const int8_t *
+cunder_tensor_accessor_i8(const Cunder_Tensor *tensor)
+{
+	return tensor->tensor.data_ptr<int8_t>();
+}
+
+const int16_t *
+cunder_tensor_accessor_i16(const Cunder_Tensor *tensor)
+{
+	return tensor->tensor.data_ptr<int16_t>();
+}
+
+const int32_t *
+cunder_tensor_accessor_i32(const Cunder_Tensor *tensor)
+{
+	return tensor->tensor.data_ptr<int32_t>();
+}
+
+const int64_t *
+cunder_tensor_accessor_i64(const Cunder_Tensor *tensor)
+{
+	return tensor->tensor.data_ptr<int64_t>();
+}
+
+const float *
+cunder_tensor_accessor_f32(const Cunder_Tensor *tensor)
+{
+	return tensor->tensor.data_ptr<float>();
+}
+const double *
+cunder_tensor_accessor_f64(const Cunder_Tensor *tensor)
+{
+	return tensor->tensor.data_ptr<double>();
+}
 
 } // extern "C"
--- Cunder/c_libtorch.h
+++ Cunder/c_libtorch.h
@@ -4,54 +4,109 @@
 #include <stdint.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 // export and deprecated definitions
 #ifdef CUNDER_STATIC_DEFINE
-#  define CUNDER_EXPORT
-#  define CUNDER_DEPRECATED
-#  define CUNDER_DEPRECATED_EXPORT
+#define CUNDER_EXPORT
+#define CUNDER_DEPRECATED
+#define CUNDER_DEPRECATED_EXPORT
 #else
-#  ifndef CUNDER_EXPORT
-#    if defined(_WIN32)
-#      ifdef CUNDER_COMPILE_LIBRARY
-#        define CUNDER_EXPORT __declspec(dllexport)
-#      else
-#        define CUNDER_EXPORT __declspec(dllimport)
-#      endif // CUNDER_COMPILE_LIBRARY
-#    else
-#      define CUNDER_EXPORT __attribute__((visibility("default")))
-#    endif // defined(_WIN32)
-#  endif // CUNDER_EXPORT
+#ifndef CUNDER_EXPORT
+#if defined(_WIN32)
+#ifdef CUNDER_COMPILE_LIBRARY
+#define CUNDER_EXPORT __declspec(dllexport)
+#else
+#define CUNDER_EXPORT __declspec(dllimport)
+#endif // CUNDER_COMPILE_LIBRARY
+#else
+#define CUNDER_EXPORT __attribute__((visibility("default")))
+#endif // defined(_WIN32)
+#endif // CUNDER_EXPORT
 #
-#  ifndef CUNDER_DEPRECATED
-#    define CUNDER_DEPRECATED __declspec(deprecated)
-#  endif // CUNDER_DEPRECATED
+
+#ifndef CUNDER_DEPRECATED
+#define CUNDER_DEPRECATED __declspec(deprecated)
+#endif // CUNDER_DEPRECATED
 #
-#  ifndef CUNDER_DEPRECATED_EXPORT
-#    define CUNDER_DEPRECATED_EXPORT CUNDER_EXPORT CUNDER_DEPRECATED
-#  endif // CUNDER_DEPRECATED_EXPORT
+
+#ifndef CUNDER_DEPRECATED_EXPORT
+#define CUNDER_DEPRECATED_EXPORT CUNDER_EXPORT CUNDER_DEPRECATED
+#endif // CUNDER_DEPRECATED_EXPORT
 #endif // CUNDER_STATIC_DEFINE
 
-
-
-// library start
-
 // definitions
-#define CUNDER_TENSOR_MAX_DIM 5
 
 // data
-struct Torch_Version
+typedef struct
 {
 	int major;
 	int minor;
 	int patch;
-};
+} Torch_Version;
+
+typedef enum
+{
+	Cunder_Uint8,
+	Cunder_Int8,
+	Cunder_Int16,
+	Cunder_Int32,
+	Cunder_Int64,
+	Cunder_Float32,
+	Cunder_Float64,
+	Cunder_Invalid
+} Cunder_DType;
+
+typedef struct Cunder_Tensor Cunder_Tensor;
 
 // API
-CUNDER_EXPORT Torch_Version
-c_torch_version();
+CUNDER_EXPORT Torch_Version cunder_torch_version();
+
+// Delete Tensor object.
+CUNDER_EXPORT int cunder_tensor_free(Cunder_Tensor *obj);
+
+// Initialize tensor without data
+CUNDER_EXPORT Cunder_Tensor *cunder_tensor_ones(int ndim, const int *shape, Cunder_DType dtype);
+
+CUNDER_EXPORT Cunder_Tensor *cunder_tensor_zeros(int ndim, const int *shape, Cunder_DType dtype);
+
+CUNDER_EXPORT Cunder_Tensor *cunder_tensor_eye(int n, Cunder_DType dtype);
+
+CUNDER_EXPORT Cunder_Tensor *cunder_tensor_range(int start, int end, int step, Cunder_DType dtype);
+
+// Initialize tensor with data
+
+CUNDER_EXPORT Cunder_Tensor *cunder_tensor_from_data_wrap(int ndim, const int *shape, void *data, Cunder_DType dtype);
+
+CUNDER_EXPORT Cunder_Tensor *cunder_tensor_from_data(int ndim, const int *shape, void *data, Cunder_DType dtype, void (*free)(void *));
+
+// Tensor to()
+
+CUNDER_EXPORT void cunder_tensor_to(Cunder_Tensor *tensor, Cunder_DType dtype);
+
+// Tensor print()
+
+CUNDER_EXPORT void cunder_tensor_print(const Cunder_Tensor *tensor);
+
+// Tensor info
+
+CUNDER_EXPORT Cunder_DType cunder_tensor_type(const Cunder_Tensor *tensor);
+CUNDER_EXPORT int64_t cunder_tensor_ndim(const Cunder_Tensor *tensor);
+CUNDER_EXPORT void cunder_tensor_shape(const Cunder_Tensor *tensor, int64_t ndim, int64_t* out_shape);
+CUNDER_EXPORT int64_t cunder_tensor_numel(const Cunder_Tensor *tensor);
+CUNDER_EXPORT int64_t cunder_tensor_dim_size(const Cunder_Tensor *tensor, int64_t dim);
+
+// tensor accessors
+
+CUNDER_EXPORT const uint8_t* cunder_tensor_accessor_u8(const Cunder_Tensor *tensor);
+CUNDER_EXPORT const int8_t* cunder_tensor_accessor_i8(const Cunder_Tensor *tensor);
+CUNDER_EXPORT const int16_t* cunder_tensor_accessor_i16(const Cunder_Tensor *tensor);
+CUNDER_EXPORT const int32_t* cunder_tensor_accessor_i32(const Cunder_Tensor *tensor);
+CUNDER_EXPORT const int64_t* cunder_tensor_accessor_i64(const Cunder_Tensor *tensor);
+CUNDER_EXPORT const float* cunder_tensor_accessor_f32(const Cunder_Tensor *tensor);
+CUNDER_EXPORT const double* cunder_tensor_accessor_f64(const Cunder_Tensor *tensor);
 
 #ifdef __cplusplus
 }
