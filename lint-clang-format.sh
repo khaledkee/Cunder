0bdbbee400134767b47fd8f403b35d70c546cdbc
Youssef Kamal <112625717+yousefelkilany@users.noreply.github.com>
2023-03-06 18:05:37 +0000

    scan `*.c` files for linting

--- lint-clang-format.sh
+++ lint-clang-format.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-git ls-files *.cpp *.h :!:external | xargs clang-format -style=file -i
+git ls-files *.c *.cpp *.h :!:external | xargs clang-format -style=file -i
 if git diff --exit-code; then
 	echo -e "[${GREEN}OK${NC}]: lint-clang-format.sh"
 else
