6ab37975b5da4a98cd39f448bd419e42af06c427
Youssef Kamal <112625717+yousefelkilany@users.noreply.github.com>
2023-03-06 17:58:45 +0000

    add gh actions unittest & format

---
+++ lint-clang-format.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+git ls-files *.cpp *.h :!:external | xargs clang-format -style=file -i
+if git diff --exit-code; then
+	echo -e "[${GREEN}OK${NC}]: lint-clang-format.sh"
+else
+	echo -e "[${RED}FAIL${NC}]: lint-clang-format.sh"
+	exit -1
+fi
